0cf3fe4a0f117e88618c93a1f5f561beab804e59
butterriolu <derpvin12@gmail.com>
2022-01-08 23:09:38 -0500

    stored variables to array for storage, added difference div to output

--- genshin_calc/script.js
+++ genshin_calc/script.js
@@ -1,51 +1,79 @@
+//indexes for array
+var CharacterLevel=0
+var BaseAttack=1;
+var TotalAttack=2;
+var EnergyRecharge=3;
+var EM=4;
+var CritRate=5;
+var CritDamage=6;
+var SkillElement=7;
+var DmgBonus=8;
+var SkillScaling=9;
+var BurstScaling=10;
+var reaction=11;
+var CharOther=12;
+var CharSkill=13;
+var CharBurst=14;
+
+var SkillNoCrit=15;
+var SkillCritHit=16;
+var SkillAverage=17;
+var BurstNoCrit=18;
+var BurstCritHit=19;
+var BurstAverage=20;
+
+let stat1={};
+let stat2={};
+let stats=[stat1,stat2];
+stats[0][0]=100;
 function calculate(ID,num){//num is div id
     // alert("Character level is: "+document.getElementById("BATK").value);
     // all variables needed for calculation
 //front stats
 // alert(document.querySelector(`.${ID} > #lv`).value); 
-    
-    var CharacterLevel= parseFloat(document.querySelector(`.${ID} > #lv`).value);//character level
-    var BaseAttack= parseInt(document.querySelector(`.${ID} > #BATK`).value);//base attack for bonuses
-    var TotalAttack= parseFloat(document.querySelector(`.${ID} > #FATK`).value);//total attack
-    var EnergyRecharge=parseInt(document.querySelector(`.${ID} > #ER`).value);//energy recharge
+var build=num-1;
+    stats[build][CharacterLevel]= parseFloat(document.querySelector(`.${ID} > #lv`).value);//character level
+    stats[build][BaseAttack]= parseInt(document.querySelector(`.${ID} > #BATK`).value);//base attack for bonuses
+    stats[build][TotalAttack]= parseFloat(document.querySelector(`.${ID} > #FATK`).value);//total attack
+    stats[build][EnergyRecharge]=parseInt(document.querySelector(`.${ID} > #ER`).value);//energy recharge
     
 //crit
-    var CritRate=parseFloat(document.querySelector(`.${ID} > #CR`).value)*.01;
-    var CritDamage=parseFloat(document.querySelector(`.${ID} > #CD`).value)*.01+1;
+    stats[build][CritRate]=parseFloat(document.querySelector(`.${ID} > #CR`).value)*.01;
+    stats[build][CritDamage]=parseFloat(document.querySelector(`.${ID} > #CD`).value)*.01+1;
 
 
 //dmg bonuses
-    var DmgBonus=parseFloat(document.querySelector(`.${ID} > #DMGBonus`).value)*.01;//damage bonus
-    var SkillScaling=parseFloat(document.querySelector(`.${ID} > #SkillScaling`).value)/100;//skill scaling
-    var BurstScaling=parseFloat(document.querySelector(`.${ID} > #BurstScaling`).value)/100;//burst scaling
+    stats[build][DmgBonus]=parseFloat(document.querySelector(`.${ID} > #DMGBonus`).value)*.01;//damage bonus
+    stats[build][SkillScaling]=parseFloat(document.querySelector(`.${ID} > #SkillScaling`).value)/100;//skill scaling
+    stats[build][BurstScaling]=parseFloat(document.querySelector(`.${ID} > #BurstScaling`).value)/100;//burst scaling
 
 
 
 //---<FIXED>---identified problem: melt undercalculates dmg, needs boost
 //reaction
-    var reaction= "";
+    stats[build][reaction]= "";
     
 //enemy stats
-    
     var EnemyLevel=parseFloat(document.getElementById("eLv").value);
     var EnemyDefense=5*EnemyLevel+500;
     var Resistance= parseFloat(document.getElementById("resist").value)*.01;
     var DefReduce=parseFloat(document.getElementById('DefShred').value)*.01;
     
-    var DMGReduction=(CharacterLevel+100)/(CharacterLevel+EnemyLevel+200);
+    var DMGReduction=(stats[build][CharacterLevel]+100)/(stats[build][CharacterLevel]+EnemyLevel+200);
     var DefMultiplier=DMGReduction;
     if(DefReduce>0){
         EnemyDefense-=DefReduce*EnemyDefense;
-        DMGReduction=EnemyDefense/(EnemyDefense+5*CharacterLevel+500);
+        DMGReduction=EnemyDefense/(EnemyDefense+5*stats[build][CharacterLevel]+500);
         DefMultiplier=1-DMGReduction;
     }
     
     var OtherBonus=parseFloat(document.getElementById("other").value)*.01;//adding other boosts ex: from constellations
     var SkillBonus=parseFloat(document.getElementById("otherSkill").value)*.01;
     var BurstBonus=parseFloat(document.getElementById("otherBurst").value)*.01;
-    var CharOther=parseFloat(document.querySelector(`.${ID}> #otherx`).value)*.01;
-    var CharSkill=parseFloat(document.querySelector(`.${ID}> #otherxS`).value)*.01;
-    var CharBurst=parseFloat(document.querySelector(`.${ID}> #otherxB`).value)*.01;
+    
+    stats[build][CharOther]=parseFloat(document.querySelector(`.${ID}> #otherx`).value)*.01;
+    stats[build][CharSkill]=parseFloat(document.querySelector(`.${ID}> #otherxS`).value)*.01;
+    stats[build][CharBurst]=parseFloat(document.querySelector(`.${ID}> #otherxB`).value)*.01;
     var AtkBonus=parseFloat(document.querySelector(`#otherAtk`).value)*.01;
     var EMBonus=parseFloat(document.querySelector(`#otherEM`).value);
         //elemental reaction 
@@ -56,7 +84,7 @@ function calculate(ID,num){//num is div id
     var ResShred=parseFloat(document.getElementById("resShred").value)*.01;//count resistance shred like 4pc VV
     
     if(document.getElementById('4VV').checked){
-        if(SkillElement==='Anemo'||SkillElement==='Geo'||SkillElement==='Physical'){
+        if(stats[build][SkillElement]==='Anemo'||stats[build][SkillElement]==='Geo'||stats[build][SkillElement]==='Physical'){
         }
         else{
             ResShred+=.4;
@@ -68,14 +96,14 @@ function calculate(ID,num){//num is div id
     //change class name to div id somehow
 
     //done
-    //changeBG(SkillElement,document.querySelector(`.${ID}`).id);
+    //changeBG(stats[build][SkillElement],document.querySelector(`.${ID}`).id);
 
     //element of skill and target
-    var SkillElement=document.querySelector(`.${ID}> #DmgELE`).value;//element of the skill
+    stats[build][SkillElement]=document.querySelector(`.${ID}> #DmgELE`).value;//element of the skill
     
     var ElementTarget=document.getElementById("AELE").value;//element on target
 
-    var EM= parseFloat(document.querySelector(`.${ID} > #EM`).value)+EMBonus;//Elemental Mastery
+    stats[build][EM]= parseFloat(document.querySelector(`.${ID} > #EM`).value)+EMBonus;//Elemental Mastery
     var VapMelt=0;
     if(document.getElementById("4witch").checked){
         VapMelt+=.15;
@@ -83,10 +111,10 @@ function calculate(ID,num){//num is div id
     //sucrose em buff
     if(document.getElementById('Sucrose').checked){
         if(document.getElementById('MollisFavonius').checked){
-            EM+=.2*parseFloat(document.getElementById('SucroseEM').value);
+            stats[build][EM]+=.2*parseFloat(document.getElementById('SucroseEM').value);
         }
         if(document.getElementById('CatalystConversion').checked){
-            EM+=50;
+            stats[build][EM]+=50;
         }
     }
     var KazuEM=parseFloat(document.getElementById('KazEM').value);
@@ -94,13 +122,13 @@ function calculate(ID,num){//num is div id
     if(document.getElementById('Kazuha').checked){
         if(document.getElementById('C2').checked){
             KazuEM+=200;
-            EM+=200;
+            stats[build][EM]+=200;
         }
         if(document.getElementById('PoF').checked){
-            if(SkillElement==='Anemo'||SkillElement==='Geo'||SkillElement==='Physical'){
+            if(stats[build][SkillElement]==='Anemo'||stats[build][SkillElement]==='Geo'||stats[build][SkillElement]==='Physical'){
             }
             else{
-                DmgBonus+=KazuEM*.0004;
+                stats[build][DmgBonus]+=KazuEM*.0004;
             }
         }
         
@@ -109,10 +137,10 @@ function calculate(ID,num){//num is div id
     var bennetBase=parseFloat(document.getElementById('bennettBase').value);
     var bennetBonus=parseFloat(document.getElementById('%bonus').value)*.01;
     if(document.getElementById('bennett').checked){
-        TotalAttack+=bennetBase*bennetBonus;
+        stats[build][TotalAttack]+=bennetBase*bennetBonus;
         if(document.getElementById('BennettC6').checked){
-            if(SkillElement=='Pyro'){
-                DmgBonus+=.15;
+            if(stats[build][SkillElement]=='Pyro'){
+                stats[build][DmgBonus]+=.15;
             }
         }
     }
@@ -120,64 +148,64 @@ function calculate(ID,num){//num is div id
         BurstBonus+=.2;
     }
     if(document.getElementById('4noblesse').checked){
-        TotalAttack+=.2*BaseAttack;
+        stats[build][TotalAttack]+=.2*stats[build][BaseAttack];
     }
 
     if(document.getElementById('Totm').checked){
-        TotalAttack+=.2*BaseAttack;
+        stats[build][TotalAttack]+=.2*stats[build][BaseAttack];
     }
 
     if(document.getElementById('emblem').checked){
-        if(EnergyRecharge>=300){
+        if(stats[build][EnergyRecharge]>=300){
             BurstBonus+=.75;
         }
         else{
-            BurstBonus+=.25*EnergyRecharge*.01;
+            BurstBonus+=.25*stats[build][EnergyRecharge]*.01;
         }
     }
     if(document.getElementById('archaic').checked){
-        DmgBonus+=.35;
+        stats[build][DmgBonus]+=.35;
     }
     if(document.getElementById('Lavawalk').checked){
         if(ElementTarget=='Pyro'){ 
-            DmgBonus+=.35;
+            stats[build][DmgBonus]+=.35;
         }
     }
     if(document.getElementById('thunderSooth').checked){
         if(ElementTarget=='Electro'){ 
-            DmgBonus+=.35;
+            stats[build][DmgBonus]+=.35;
         }
     }
     if(document.getElementById('4instructor').checked){
-        EM+=120;
+        stats[build][EM]+=120;
     }
 
     if(document.getElementById('adeptus').checked){
-        TotalAttack+=371;
-        CritRate+=.12;
+        stats[build][TotalAttack]+=371;
+        stats[build][CritRate]+=.12;
     }
         
     if(document.getElementById('potion').checked){
-        DmgBonus+=.25;
+        stats[build][DmgBonus]+=.25;
     }
     if(document.getElementById('NoTomorrow').checked){
-        CritRate+=.2;
-        CritDamage+=.2;
+        stats[build][CritRate]+=.2;
+        stats[build][CritDamage]+=.2;
     }
 
     if(document.getElementById('thrillingTales').checked){
-        TotalAttack+=.48*BaseAttack;
+        stats[build][TotalAttack]+=.48*stats[build][BaseAttack];
     }
 
     //something might be wrong with geoResonance, test it.
     if(document.getElementById('geoRes').checked){
-        DmgBonus+=.15;
-        if(SkillElement=="Geo"){
+        stats[build][DmgBonus]+=.15;
+        if(stats[build][SkillElement]=="Geo"){
             ResShred+=.2;
         }
     }
     if(document.getElementById('supcon').checked){
-        if(SkillElement=='Physical'){
+        if(stats[build][SkillElement]=='Physical'){
             ResShred+=.4;
         }
     }
@@ -186,45 +214,45 @@ function calculate(ID,num){//num is div id
     
     
     //final em calculation
-    VapMelt+= parseFloat((2.78*EM)/(EM+1400));//Melt/ Vaporize bonus
-    var ReactionBonus=ElementalReaction(SkillElement,ElementTarget,VapMelt);
+    VapMelt+= parseFloat((2.78*stats[build][EM])/(stats[build][EM]+1400));//Melt/ Vaporize bonus
+    var ReactionBonus=ElementalReaction(build,stats[build][SkillElement],ElementTarget,VapMelt);
 
     if(document.getElementById('Mona').checked){
-        DmgBonus+=parseFloat(document.getElementById('omen%').value)*.01;
+        stats[build][DmgBonus]+=parseFloat(document.getElementById('omen%').value)*.01;
         if(document.getElementById('MonaC1').checked){
-            if(reaction==="Vaporize"){
+            if(stats[build][reaction]==="Vaporize"){
                 VapMelt+=.15;
             }
         }
     }
     if(document.getElementById('Sara').checked){
-        TotalAttack+=parseFloat(document.getElementById('SaraBase').value)*
+        stats[build][TotalAttack]+=parseFloat(document.getElementById('SaraBase').value)*
         parseFloat(document.getElementById('sara%bonus').value)*.01;
         if(document.getElementById('SaraC6').checked){
-            if(SkillElement=='Electro'){
-                CritDamage+=.6;
+            if(stats[build][SkillElement]=='Electro'){
+                stats[build][CritDamage]+=.6;
             }
         }
     }
 
 //final calculation
     
-    DmgBonus+=OtherBonus+CharOther;
+stats[build][DmgBonus]+=OtherBonus+stats[build][CharOther];
 //bug w/ other dmg bonuses, hutao w/ 0 is good
 // diluc w/ 15% is .18% off
 //chongyun w/ 60% is 9.6%
 //found out why: noblesse is meant to be in dmg bonus, not dmg scaling
-    TotalAttack+=AtkBonus*BaseAttack;
+stats[build][TotalAttack]+=AtkBonus*stats[build][BaseAttack];
     
     
 //bonus scaling
     var BonusScale=0;
     BonusScale+=parseFloat(document.getElementById('bonusFlatScaling').value);
 
-    var SkillTotal=TotalAttack*SkillScaling+BonusScale;
-    var BurstTotal=TotalAttack*BurstScaling+BonusScale;
+    var SkillTotal=stats[build][TotalAttack]*stats[build][SkillScaling]+BonusScale;
+    var BurstTotal=stats[build][TotalAttack]*stats[build][BurstScaling]+BonusScale;
     if(document.getElementById('ShenHe').checked){
-        if(SkillElement==='Cryo'){
+        if(stats[build][SkillElement]==='Cryo'){
             var ShenHeATK=parseFloat(document.getElementById('ShenHeATK').value);
             var ShenHeScale=parseFloat(document.getElementById('ShenHe%Bonus').value)*.01;
             SkillTotal+=ShenHeATK*ShenHeScale;
@@ -233,11 +261,11 @@ function calculate(ID,num){//num is div id
         if(document.getElementById('ShenHeBurst').checked){
             ResShred+=parseFloat(document.getElementById('ShenHeResShred').value*.01);
         }
-        if(document.getElementById("ShenHeA1").checked&&SkillElement==='Cryo'){
-            DmgBonus+=.15;
+        if(document.getElementById("ShenHeA1").checked&&stats[build][SkillElement]==='Cryo'){
+            stats[build][DmgBonus]+=.15;
         }
         if(document.getElementById("ShenHeA4").checked){
-            DmgBonus+=.15;
+            stats[build][DmgBonus]+=.15;
         }
 
     }
@@ -247,14 +275,20 @@ function calculate(ID,num){//num is div id
     var ResMultiplier= ResistanceCalc(ResPercent);//get actual multiplier
     var DMGreduced=DefMultiplier*ResMultiplier*ReactionBonus;//excluding dmg scaling
 
-    var SkillOut=SkillTotal*DMGreduced*(1+DmgBonus+SkillBonus+CharSkill);
-    var SkillCrit=SkillOut*(CritDamage);
-    var Skillavg=SkillOut*(1-CritRate)+SkillCrit*CritRate;
+    var SkillOut=SkillTotal*DMGreduced*(1+stats[build][DmgBonus]+SkillBonus+stats[build][CharSkill]);
+    stats[build][SkillNoCrit]=SkillOut;
+    var SkillCrit=SkillOut*(stats[build][CritDamage]);
+    stats[build][SkillCritHit]=SkillCrit;
+    var Skillavg=SkillOut*(1-stats[build][CritRate])+SkillCrit*stats[build][CritRate];
+    stats[build][SkillAverage]=Skillavg;
 
-    var BurstOut=BurstTotal*DMGreduced*(1+DmgBonus+BurstBonus+CharBurst);
-    var BurstCrit=BurstOut*(CritDamage);
-    var Burstavg=BurstOut*(1-CritRate)+BurstCrit*CritRate;
-
+    var BurstOut=BurstTotal*DMGreduced*(1+stats[build][DmgBonus]+BurstBonus+stats[build][CharBurst]);
+    stats[build][BurstNoCrit]=BurstOut;
+    var BurstCrit=BurstOut*(stats[build][CritDamage]);
+    stats[build][BurstCritHit]=BurstCrit;
+    var Burstavg=BurstOut*(1-stats[build][CritRate])+BurstCrit*stats[build][CritRate];
+    stats[build][BurstAverage]=Burstavg;
+    
 
 
     // document.getElementById("output").textContent='non-crit hit:\t'
@@ -264,19 +298,19 @@ function calculate(ID,num){//num is div id
 
     // //detailed console calculation
     document.querySelector(`#console${num}`).innerHTML=
-    'Character level:\t\t'+CharacterLevel+
-    '\nAttack:\t\t\t\t'+(TotalAttack.toFixed(1)||0)+
+    'Level:\t\t'+stats[build][CharacterLevel]+
+    '\nAttack:\t\t'+(stats[build][TotalAttack].toFixed(1)||0)+
     //'\nAdditive Damage:\t\t'+AddBonus*AddPercent+
-    '\nElemental Mastery:\t'+EM
-    +'\nMelt/ Vaporize Bonus:'+((VapMelt*100).toFixed(1)||0)+'%'
-    +'\nCrit Rate:\t\t\t'+(CritRate*100).toFixed(1)+'%\nCrit Damage:\t\t\t'+((CritDamage-1)*100).toFixed(1)+'%'
-    +'\nTarget is affected by: \t'+ElementTarget+'\nDamage Element is: \t'+SkillElement
-    +'\nEnergyRecharge%:\t'+(EnergyRecharge).toFixed(1)+'%\nDamage Bonus:\t\t'+(DmgBonus*100).toFixed(1)+'%'
-    +'\nReactionBonus:\t\t'+ReactionBonus.toFixed(2)
-    +'\nEnemy Level:\t\t\t'+EnemyLevel+'\nEnemy Defense:\t\t'+EnemyDefense.toFixed(2)+'\nResistance Multiplier:'+ResMultiplier.toFixed(2)
-    +'\nBonus FlatDamage\t\t'+BonusScale
-    +'\nSkill Out:\t\t\t'+SkillOut.toFixed(1)+"\nDMG Reduction:\t\t"+DMGReduction.toFixed(3)
-    +'\nDEF Multiplier:\t\t'+DefMultiplier.toFixed(3);
+    '\nEle Mastery:\t'+stats[build][EM]
+    +'\nMelt/ Vap:\t'+((VapMelt*100).toFixed(1)||0)+'%'
+    +'\nCrit Rate:\t'+(stats[build][CritRate]*100).toFixed(1)+'%\nCrit Damage:\t'+((stats[build][CritDamage]-1)*100).toFixed(1)+'%'
+    +'\nTarget ELE: \t'+ElementTarget+'\nDMG ELE: '+stats[build][SkillElement]
+    +'\nER%:\t\t'+(stats[build][EnergyRecharge]).toFixed(1)+'%\nDMG Bonus:\t'+(stats[build][DmgBonus]*100).toFixed(1)+'%'
+    +'\nReaction:\t'+ReactionBonus.toFixed(2)
+    +'\nEnemy Level:\t'+EnemyLevel+'\nEnemy DEF:\t'+EnemyDefense.toFixed(2)+'\nResistance:'+ResMultiplier.toFixed(2)
+    +'\nBonus Flat\t'+BonusScale
+    +'\nSkill Out:\t'+SkillOut.toFixed(1)+"\nDMG Reduce:"+DMGReduction.toFixed(3)
+    +'\nDEF Multi:\t'+DefMultiplier.toFixed(3);
 
     document.querySelector(`#skill${num}`).innerHTML=
     'SKILL DAMAGE\nNon-Crit:\t'+ SkillOut.toFixed(0)
@@ -287,6 +321,20 @@ function calculate(ID,num){//num is div id
     'BURST DAMAGE\nNon-Crit:  \t'+ BurstOut.toFixed(0)
     +'\nCrit Hit:  \t'+BurstCrit.toFixed(0)
     +'\nAverage:  \t'+Burstavg.toFixed(0);
+
+    //DIFFERENCE DISPLAY
+    document.querySelector(`#diffS`).innerHTML=
+    'Skill DIFF\nNon-Crit:  \t'+ (stats[1][SkillNoCrit]-stats[0][SkillNoCrit]).toFixed(0)
+    +'\nCrit DIFF:  \t'+(stats[1][SkillCritHit]-stats[0][SkillCritHit]).toFixed(0)
+    +'\nAVG DIFF :  \t'+(stats[1][SkillAverage]-stats[0][SkillAverage]).toFixed(0);
+    document.querySelector(`#diffB`).innerHTML=
+    'BURST DIFF\nNon-Crit:  \t'+ (stats[1][BurstNoCrit]-stats[0][BurstNoCrit]).toFixed(0)
+    +'\nCrit DIFF:  \t'+(stats[1][BurstCritHit]-stats[0][BurstCritHit]).toFixed(0)
+    +'\nAVG DIFF :  \t'+(stats[1][BurstAverage]-stats[0][BurstAverage]).toFixed(0);
+
+    // stats[build][SkillNoCrit]=SkillOut;
+    // stats[build][SkillCritHit]=SkillCrit;
+    // stats[build][SkillAverage]=Skillavg;
     // document.getElementById('console2').style.display='inline-block';
     // document.getElementById('skill2').style.display='inline-block';
     // document.getElementById('burst2').style.display='inline-block';
@@ -307,21 +355,21 @@ function unCheck(checkbox){
     document.getElementById(checkbox).checked=false;
 }
 
-function ElementalReaction(skill, target, VapMelt){
+function ElementalReaction(build,skill, target, VapMelt){
     if (skill==='Pyro'&&target==='Cryo'){
-        reaction="Melt";
+        stats[build][reaction]="Melt";
         return 2*(1+VapMelt);
     }
     else if(skill==='Cryo'&&target==='Pyro'){
-        reaction="Melt";
+        stats[build][reaction]="Melt";
         return 1.5*(1+VapMelt);
     }
     else if(skill==='Pyro'&&target==='Hydro'){
-        reaction="Vaporize";
+        stats[build][reaction]="Vaporize";
         return 1.5*(1+VapMelt);
     }
     else if(skill==='Hydro'&&target==='Pyro'){
-        reaction="Vaporize";
+        stats[build][reaction]="Vaporize";
         return 2*(1+VapMelt);
     }
     else{
@@ -468,6 +516,7 @@ function loadBody() {
   }
   
   function updateHtmlImport(configArr) {
+      //update this for 
     CharacterLevel = configArr[0];
     BaseAttack = configArr[1];
     TotalAttack = configArr[2];
